afb87a0bc146f58e4c65004a1d57c8299480b4a6
Murilo Preto <87781027+murilo-preto@users.noreply.github.com>
2023-06-20 14:36:45 -0300

    realtime fex display

--- raspberryPi/main.py
+++ raspberryPi/main.py
@@ -16,13 +16,30 @@ from tkVideoPlayer import TkinterVideo
 import os
 from websockets.sync.client import connect
 import json
+import functools
 
-# Conexão ws com servidor
-with connect("ws://localhost:3000") as websocket:
+def play_next_video(event, videoplayer, playlist):
+    fex = receiveData(websocket)
+    fexVideo = full_path+"/"+expressaoAtual+expressaoFacial2Code[fex]+".mp4"
+    queue.append(fexVideo)
+
+    next_video = next(playlist, None)
+    print('Next is:', next_video)
+    if not next_video:
+        return
+
+    # Play new video only after end of current
+    videoplayer.load(next_video)
+    videoplayer.play()
+
+def receiveData(websocket):
     receivedData = websocket.recv()
     receivedData = json.loads(receivedData.strip("[]"))
     fex = receivedData["fex"]
-    
+    print(fex)
+
+    return fex
+
 # Anota todos os nomes de videos em uma lista
 absPath = os.path.dirname(__file__)
 relativePath = "videoFiles"
@@ -33,18 +50,37 @@ for diretorio, _, arquivos in os.walk(full_path):
     for arquivo in arquivos:
         nomeVideos.append(arquivo)
 
-# Converte expressao facial em ingles para o codigo adotado
 expressaoFacial2Code = {"happy":"F", "neutral":"N", "doubt":"D", "surprise":"S"}
-expressaoAtual = ""
-expressaoNova = ""
+expressaoAtual = "N"
+queue = []
 
-# Interface para execucao de video
-root = tk.Tk()
-root.geometry("800x500")
+# Conexão ws com servidor
+with connect("ws://localhost:3000") as websocket:
+    fex = receiveData(websocket)
+    fexVideo = full_path+"/"+expressaoAtual+expressaoFacial2Code[fex]+".mp4"
+    queue.append(fexVideo)
 
-videoplayer = TkinterVideo(master=root, scaled=False)
-videoplayer.pack(expand=True, fill="both")
-videoplayer.load(full_path + "/DF.mp4")
-videoplayer.play()
+    if __name__ == "__main__":
+        root = tk.Tk()
+        root.geometry("800x500")
+        videoplayer = TkinterVideo(master=root, scaled=True)
 
-root.mainloop()
+        # Use 'iter' to work with 'next' in callback
+        playlist = iter(queue)
+
+        # After end of video, use callback to run next video from playlist
+        videoplayer.bind(
+            "<<Ended>>",
+            functools.partial(
+                play_next_video,
+                videoplayer=videoplayer,
+                playlist=playlist,
+            ),
+        )
+
+    # Play first video from playlist
+    videoplayer.load(next(playlist))
+    videoplayer.pack(expand=True, fill="both")
+    videoplayer.play()
+
+    root.mainloop()
