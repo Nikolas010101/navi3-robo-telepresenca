9b94b6cebea074bfb14fbc53c2274c7898985b56
Nikolas010101 <80547361+Nikolas010101@users.noreply.github.com>
2023-08-27 19:11:23 -0300

    create face_position.py
    Gets center of first detected face (if any)

---
+++ serverRoboTelepresenca/face_detection/face_position.py
@@ -0,0 +1,30 @@
+import cv2
+
+face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+cap = cv2.VideoCapture(0)
+
+
+# Load functions
+def make_rect(img):
+    faces = face_detector.detectMultiScale(img, 1.3, 5)
+
+    if not len(faces):
+        return img
+
+    x, y, w, h = faces[0]
+    print(f"Centro do rosto em: {x+w/2}, {y+h/2}")
+    face_with_rect = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    return face_with_rect
+
+
+while True:
+    ret, frame = cap.read()
+    face = cv2.resize(make_rect(frame), (400, 400))
+
+    cv2.imshow("Rosto", face)
+
+    if cv2.waitKey(1) == 13:  # espaço
+        break
+
+cap.release()
+cv2.destroyAllWindows()
