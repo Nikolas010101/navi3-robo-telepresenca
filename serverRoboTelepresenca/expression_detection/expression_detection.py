3855a15b9b4e4462b612274b978c6031a70f6ee5
Nikolas010101 <80547361+Nikolas010101@users.noreply.github.com>
2023-09-23 18:07:55 -0300

    multiple changes
    add IP and INTERVAL arguments
    change data transmission from websocket to standard out

--- serverRoboTelepresenca/expression_detection/expression_detection.py
+++ serverRoboTelepresenca/expression_detection/expression_detection.py
@@ -1,9 +1,12 @@
-import cv2, numpy as np, urllib.request as urllib, json
+import cv2, numpy as np, urllib.request as urllib, json, sys
 from rmn import RMN
-from websockets.sync.client import connect
-from time import time
+from time import sleep
 
-face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+FACEDETECTORPATH = ""
+INTERFACEIP = sys.argv[1]
+INTERVAL = float(sys.argv[2])
+
+face_detector = cv2.CascadeClassifier(FACEDETECTORPATH)
 rmn = RMN()
 
 FEX_MAP = {
@@ -15,42 +18,37 @@ FEX_MAP = {
     "surprise": "S",
     "neutral": "N",
 }
-INTERFACE_VIDEO = "http://192.168.0.100:8080/video"
-INTERVAL = 5
+# INTERFACE_VIDEO = "http://192.168.0.100:8080/video"
+INTERFACE_VIDEO = f"http://{INTERFACEIP}:8080/video"
 
-with connect("ws://192.168.0.100:3000") as websocket:
-    last = time()
-    while True:
-        try:
-            current = time()
-            if current - last < INTERVAL:
-                continue
-            last = time()
-            with urllib.urlopen(INTERFACE_VIDEO) as resp:
-                img_array = np.asarray(bytearray(resp.read()), dtype=np.uint8)
-                colored = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+while True:
+    try:
+        with urllib.urlopen(INTERFACE_VIDEO) as resp:
+            img_array = np.asarray(bytearray(resp.read()), dtype=np.uint8)
+            colored = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
-            gray_scale = cv2.cvtColor(colored, cv2.COLOR_BGR2GRAY)
-            faces = face_detector.detectMultiScale(
-                gray_scale, scaleFactor=1.1, minNeighbors=4
-            )
-            if not len(faces):
-                continue
-
-            x, y, w, h = faces[0]
-            cropped = colored[y : y + h, x : x + w]
-            detectedFex = rmn.detect_emotion_for_single_face_image(cropped)
-            print(FEX_MAP[detectedFex[0]])
-            center = x + w / 2, y + h / 2
-            websocket.send(
-                json.dumps(
-                    {
-                        "type": "face_data",
-                        "face_x": center[0],
-                        "face_y": center[1],
-                        "fex": FEX_MAP[detectedFex[0]],
-                    }
-                )
-            )
-        except:
+        gray_scale = cv2.cvtColor(colored, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(
+            gray_scale, scaleFactor=1.1, minNeighbors=4
+        )
+        if not len(faces):
             continue
+
+        x, y, w, h = faces[0]
+        cropped = colored[y : y + h, x : x + w]
+        detectedFex = rmn.detect_emotion_for_single_face_image(cropped)
+        center = x + w / 2, y + h / 2
+        print(
+            json.dumps(
+                {
+                    "type": "face_data",
+                    "face_x": center[0],
+                    "face_y": center[1],
+                    "fex": FEX_MAP[detectedFex[0]],
+                }
+            ),
+            flush=True,
+        )
+        sleep(INTERVAL)
+    except:
+        continue
