f4d350e0b5b53894e21040ad259ce4386cfbb37b
Nikolas010101 <80547361+Nikolas010101@users.noreply.github.com>
2023-09-23 18:11:12 -0300

    spawn subprocesses on startup
    spawns child processes for interface media and fex detection
    extract server setup constants to another file

--- serverRoboTelepresenca/index.js
+++ serverRoboTelepresenca/index.js
@@ -1,15 +1,16 @@
 import express from "express";
 import { WebSocket, WebSocketServer } from "ws";
 import { v4 } from "uuid";
-import os from "os";
+import { spawn } from "child_process";
+import {
+	INTERFACEMEDIAPATH,
+	FEXDETECTIONPATH,
+	INTERFACEIP,
+	FEXINTERVAL,
+} from "./public/server_setup/setup.js";
 
 const app = express();
 const port = process.env.port || 3000;
-
-// Printa o Endereço de IP do servidor + porta
-var networkInterfaces = os.networkInterfaces();
-const ip = "http://192.168.0.100";
-console.log(`Server on: ${ip}:${port}`);
 
 // Middleware
 app.set("view engine", "ejs");
@@ -17,47 +18,55 @@ app.use([express.json(), express.static("public")]);
 
 // Configuracao de servidor websocket na mesma porta do servidor web
 const wsServer = new WebSocketServer({ noServer: true });
+const interfaceMedia = spawn("python", [INTERFACEMEDIAPATH, INTERFACEIP]);
+const fexDetection = spawn("python", [
+	FEXDETECTIONPATH,
+	INTERFACEIP,
+	FEXINTERVAL,
+]);
+fexDetection.stdout.on("data", (data) => {
+	const message = JSON.parse(data.toString());
+
+	console.log(message);
+
+	state.fex = message.fex === "ND" ? "N" : message.fex;
+	distributeData({ type: "control", ...state });
+});
 const server = app.listen(port);
 
 // Handling de request do servidor soquete
 wsServer.on("connection", function (connection) {
-    const userId = v4();
-    clients[userId] = connection;
-    console.log("Server: Connection established");
+	const userId = v4();
+	clients[userId] = connection;
+	console.log("Server: Connection established");
 
-    connection.on("close", () => handleDisconnect(userId));
+	connection.on("close", () => handleDisconnect(userId));
 
-    connection.on("message", function (message) {
-        message = JSON.parse(message.toString());
-        switch (message.type) {
-            case "control":
-                console.log(message);
+	connection.on("message", function (message) {
+		message = JSON.parse(message.toString());
+		switch (message.type) {
+			case "control":
+				console.log(message);
 
-                state.pan = message.pan;
-                state.tilt = message.tilt;
-                state.fex = message.fex;
+				state.pan = message.pan;
+				state.tilt = message.tilt;
+				state.fex = message.fex;
 
-                distributeData({ type: "control", ...state });
-                break;
-            case "face_data":
-                console.log(message);
-
-                state.fex = message.fex === "ND" ? "N" : message.fex;
-                distributeData({ type: "control", ...state });
-                break;
-            default:
-                console.log(`Unsupported message type: ${message.type}`);
-                break;
-        }
-    });
-    distributeData({ type: "control", ...state });
+				distributeData({ type: "control", ...state });
+				break;
+			default:
+				console.log(`Unsupported message type: ${message.type}`);
+				break;
+		}
+	});
+	distributeData({ type: "control", ...state });
 });
 
 // Mudanca de protocolo de http para ws
 server.on("upgrade", (req, socket, head) => {
-    wsServer.handleUpgrade(req, socket, head, (ws) => {
-        wsServer.emit("connection", ws, req);
-    });
+	wsServer.handleUpgrade(req, socket, head, (ws) => {
+		wsServer.emit("connection", ws, req);
+	});
 });
 
 // clients = todos usuarios conectados ao servidor ws
@@ -65,38 +74,38 @@ const clients = {};
 
 // envia um arquivo json para todos os usuarios conectados ao servidor ws
 function distributeData(json) {
-    const data = JSON.stringify(json);
-    Object.values(clients).forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(data);
-        }
-    });
+	const data = JSON.stringify(json);
+	Object.values(clients).forEach((client) => {
+		if (client.readyState === WebSocket.OPEN) {
+			client.send(data);
+		}
+	});
 }
 
 function handleDisconnect(userId) {
-    console.log(`${userId} disconnected.`);
-    delete clients[userId];
+	console.log(`${userId} disconnected.`);
+	delete clients[userId];
 }
 
 const state = {
-    pan: 0,
-    tilt: 0,
-    fex: "N",
+	pan: 0,
+	tilt: 0,
+	fex: "N",
 };
 
 // GET
 
 // Homepage
 app.get("/", function (req, res) {
-    res.render("pages/index");
+	res.render("pages/index");
 });
 
 // Pagina para controlar sistema embarcado
 app.get("/control", function (req, res) {
-    res.render("pages/control");
+	res.render("pages/control");
 });
 
 // Pagina para mostrar as expressões faciais
 app.get("/expression", function (req, res) {
-    res.render("pages/expression");
+	res.render("pages/expression");
 });
