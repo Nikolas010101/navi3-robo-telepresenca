57297a38e319eb562a7ceae11f917fbd0a441344
Nikolas010101 <nik.grande@hotmail.com>
2023-10-04 19:12:22 -0300

    add opencv model, add logging, add paths

--- serverRoboTelepresenca/index.js
+++ serverRoboTelepresenca/index.js
@@ -3,10 +3,10 @@ import { WebSocket, WebSocketServer } from "ws";
 import { v4 } from "uuid";
 import { spawn } from "child_process";
 import {
-	INTERFACEMEDIAPATH,
-	FEXDETECTIONPATH,
-	INTERFACEIP,
-	FEXINTERVAL,
+    INTERFACEMEDIAPATH,
+    FEXDETECTIONPATH,
+    INTERFACEIP,
+    FEXINTERVAL,
 } from "./public/server_setup/setup.js";
 
 const app = express();
@@ -19,54 +19,67 @@ app.use([express.json(), express.static("public")]);
 // Configuracao de servidor websocket na mesma porta do servidor web
 const wsServer = new WebSocketServer({ noServer: true });
 const interfaceMedia = spawn("python", [INTERFACEMEDIAPATH, INTERFACEIP]);
+interfaceMedia.on("spawn", (e) =>
+    console.log("interface_media spawned successfully")
+);
+interfaceMedia.on("error", (e) =>
+    console.log("Error on interface_media subprocess")
+);
+
 const fexDetection = spawn("python", [
-	FEXDETECTIONPATH,
-	INTERFACEIP,
-	FEXINTERVAL,
+    FEXDETECTIONPATH,
+    INTERFACEIP,
+    FEXINTERVAL,
 ]);
+fexDetection.on("spawn", (e) =>
+    console.log("fex_detection spawned successfully")
+);
+fexDetection.on("error", (e) =>
+    console.log("Error on fex_detection subprocess")
+);
 fexDetection.stdout.on("data", (data) => {
-	const message = JSON.parse(data.toString());
+    const message = JSON.parse(data.toString());
 
-	console.log(message);
+    console.log(message);
 
-	state.fex = message.fex === "ND" ? "N" : message.fex;
-	distributeData({ type: "control", ...state });
+    state.fex = message.fex === "ND" ? "N" : message.fex;
+    distributeData({ type: "control", ...state });
 });
 const server = app.listen(port);
 
 // Handling de request do servidor soquete
 wsServer.on("connection", function (connection) {
-	const userId = v4();
-	clients[userId] = connection;
-	console.log("Server: Connection established");
+    const userId = v4();
+    clients[userId] = connection;
+    console.log("Server: Connection established");
 
-	connection.on("close", () => handleDisconnect(userId));
+    connection.on("close", () => handleDisconnect(userId));
 
-	connection.on("message", function (message) {
-		message = JSON.parse(message.toString());
-		switch (message.type) {
-			case "control":
-				console.log(message);
+    connection.on("message", function (message) {
+        message = JSON.parse(message.toString());
+        switch (message.type) {
+            case "control":
+                console.log(message);
 
-				state.pan = message.pan;
-				state.tilt = message.tilt;
-				state.fex = message.fex;
+                state.pan = message.pan;
+                state.tilt = message.tilt;
+                state.fex = message.fex;
 
-				distributeData({ type: "control", ...state });
-				break;
-			default:
-				console.log(`Unsupported message type: ${message.type}`);
-				break;
-		}
-	});
-	distributeData({ type: "control", ...state });
+                distributeData({ type: "control", ...state });
+                break;
+            default:
+                console.log(`Unsupported message type: ${message.type}`);
+                break;
+        }
+    });
+    distributeData({ type: "control", ...state });
 });
 
 // Mudanca de protocolo de http para ws
 server.on("upgrade", (req, socket, head) => {
-	wsServer.handleUpgrade(req, socket, head, (ws) => {
-		wsServer.emit("connection", ws, req);
-	});
+    wsServer.handleUpgrade(req, socket, head, (ws) => {
+        wsServer.emit("connection", ws, req);
+    });
 });
 
 // clients = todos usuarios conectados ao servidor ws
@@ -74,38 +87,38 @@ const clients = {};
 
 // envia um arquivo json para todos os usuarios conectados ao servidor ws
 function distributeData(json) {
-	const data = JSON.stringify(json);
-	Object.values(clients).forEach((client) => {
-		if (client.readyState === WebSocket.OPEN) {
-			client.send(data);
-		}
-	});
+    const data = JSON.stringify(json);
+    Object.values(clients).forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
 }
 
 function handleDisconnect(userId) {
-	console.log(`${userId} disconnected.`);
-	delete clients[userId];
+    console.log(`${userId} disconnected.`);
+    delete clients[userId];
 }
 
 const state = {
-	pan: 0,
-	tilt: 0,
-	fex: "N",
+    pan: 0,
+    tilt: 0,
+    fex: "N",
 };
 
 // GET
 
 // Homepage
 app.get("/", function (req, res) {
-	res.render("pages/index");
+    res.render("pages/index");
 });
 
 // Pagina para controlar sistema embarcado
 app.get("/control", function (req, res) {
-	res.render("pages/control");
+    res.render("pages/control");
 });
 
 // Pagina para mostrar as expressões faciais
 app.get("/expression", function (req, res) {
-	res.render("pages/expression");
+    res.render("pages/expression");
 });
--- serverRoboTelepresenca/public/server_setup/setup.js
+++ serverRoboTelepresenca/public/server_setup/setup.js
@@ -1,7 +1,7 @@
 export const INTERFACEMEDIAPATH =
-	"";
+	"/home/nikolas/GitHub/navi3-robo-telepresenca/interfaceMedia/media.py";
 export const FEXDETECTIONPATH =
-	"";
+	"/home/nikolas/GitHub/navi3-robo-telepresenca/serverRoboTelepresenca/expression_detection/expression_detection.py";
 export const FEXINTERVAL = 3;
 export const INTERFACEIP = "192.168.0.100";
 export const SERVERIP = "192.168.0.100";
