ac5b9fd798c44d351f3177db1c8a54050f85d6d9
Nikolas <nikolas@nikolas.com>
2023-12-30 22:29:54 -0300

    read frames from websocket

--- serverRoboTelepresenca/expression_detection/expression_detection.py
+++ serverRoboTelepresenca/expression_detection/expression_detection.py
@@ -1,20 +1,19 @@
-import cv2, numpy as np, urllib.request as urllib, json, time
-from urllib.error import URLError, HTTPError
+import cv2, numpy as np, json, time, base64
 from websockets.sync.client import connect
 from websockets.exceptions import InvalidURI, InvalidHandshake, ConnectionClosedError
 from rmn import RMN
 
-FACEDETECTORPATH = "/home/nikolas/Documents/GitHub/navi3-robo-telepresenca/serverRoboTelepresenca/expression_detection/haarcascade_frontalface_default.xml"
+FACE_DETECTOR_PATH = "/home/nikolas/Documents/GitHub/navi3-robo-telepresenca/serverRoboTelepresenca/expression_detection/haarcascade_frontalface_default.xml"
 
 with open(
     "/home/nikolas/Documents/GitHub/navi3-robo-telepresenca/serverRoboTelepresenca/public/server_setup/setup.json",
     "r",
 ) as file:
     setup: dict = json.load(file)
-    INTERFACE_IP: str = setup["INTERFACE_IP"]
     SERVER_IP: str = setup["SERVER_IP"]
+    INTERVAL: int = setup["INTERVAL"]
 
-face_detector = cv2.CascadeClassifier(FACEDETECTORPATH)
+face_detector = cv2.CascadeClassifier(FACE_DETECTOR_PATH)
 rmn = RMN()
 
 FEX_MAP = {
@@ -27,16 +26,17 @@ FEX_MAP = {
     "neutral": "N",
 }
 
-USER_VIDEO_URL = f"http://{INTERFACE_IP}:8080/video"
-
+prev = time.time()
 while True:
     try:
         with connect(f"ws://{SERVER_IP}:3000") as websocket:
             while True:
-                try:
-                    with urllib.urlopen(USER_VIDEO_URL) as resp:
-                        img_array = np.asarray(bytearray(resp.read()), dtype=np.uint8)
-                        colored = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                message = json.loads(websocket.recv())
+                curr = time.time()
+                if message["type"] == "interface_video" and curr - prev >= INTERVAL:
+                    encoded_data = message["media"]
+                    image = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
+                    colored = cv2.imdecode(image, cv2.IMREAD_COLOR)
 
                     gray_scale = cv2.cvtColor(colored, cv2.COLOR_BGR2GRAY)
                     faces = face_detector.detectMultiScale(
@@ -48,6 +48,7 @@ while True:
                     x, y, w, h = faces[0]
                     cropped = colored[y : y + h, x : x + w]
                     detectedFex = rmn.detect_emotion_for_single_face_image(cropped)
+
                     websocket.send(
                         json.dumps(
                             {
@@ -56,9 +57,8 @@ while True:
                             }
                         )
                     )
-                except (URLError, HTTPError) as e:
-                    print(f"Could not retrieve data from server, error: {e}")
-                    continue
+                    prev = time.time()
+
     except (InvalidURI, OSError, InvalidHandshake, ConnectionClosedError) as e:
         print(f"Could not connect to server, error: {e}")
         time.sleep(2)
